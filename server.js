6f2650044c9b4299a9a5bb92e507942395ded1f9
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-11-26 22:15:39 -0300

    Aula 02: Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const posts = [
+    { descricao: "Gato brincando", imagem: "https://placecats.com/millie/300/150", id: 1 },
+    { descricao: "Gato à noite", imagem: "https://placecats.com/millie/300/150", id: 2 },
+    { descricao: "Gato manhoso", imagem: "https://placecats.com/millie/300/150", id: 3 }
+  ];
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão!");
+function findPostById(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = findPostById(req.params.id);
+
+    res.status(200).json(posts[index]);
 });
